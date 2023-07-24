46931b988928067688917527e0e2271b4e1eb017
SegevBe24-meet <139688996+SegevBe24-meet@users.noreply.github.com>
2023-07-24 10:29:19 +0300

    yeah

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -8,17 +8,42 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+    try:
 
+    except:    
+    return render_template("signin.html")
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('signin'))
+        except:
+            error = "Authentication failed"
     return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     return render_template("add_tweet.html")
+
+config = {
+  "apiKey" : "AIzaSyC0NViXXtREB-z8Tau5zl5KUnk0PgB6X4w",
+  "authDomain": "segev-firebase.firebaseapp.com",
+  "projectId": "segev-firebase",
+  "storageBucket": "segev-firebase.appspot.com",
+  "messagingSenderId": "114124760344",
+  "appId": "1:114124760344:web:30a595ad1cbf744cd66a6d",
+  "measurementId": "G-BPSS8WEH38",
+  "databaseURL" : ""
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
 
 if __name__ == '__main__':
